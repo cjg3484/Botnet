91eeea1d0b5da4918f4e3cf276914da4ad918b0b
Caleb Gardner <cjg3484@utulsa.edu>
2021-11-18 13:25:01 -0600

    http serving pdf

---
+++ server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+// temporary directory location
+var srcDir = filepath.FromSlash("C:\\Users\\laugh\\Documents\\Github\\Botnet\\src")
+
+func main() {
+
+	fmt.Printf("Starting server at port 8081\n")
+
+	// return a `.pdf` file for `/pdf` route
+	http.HandleFunc("/pdf", func(res http.ResponseWriter, req *http.Request) {
+		http.ServeFile(res, req, filepath.Join(srcDir, "/files/PWNED.pdf"))
+	})
+
+	// start HTTP server with `http.DefaultServeMux` handler
+	log.Fatal(http.ListenAndServe("localhost:8081", nil))
+}
